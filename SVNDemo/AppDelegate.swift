1d4f198fa899fa7c334d80070111ee3c8043babe
Tushar Bosamiya <tushar@tushars-mac-mini.local>
2017-10-06 18:05:23 +0530

    Message by tushar

--- SVNDemo/AppDelegate.swift
+++ SVNDemo/AppDelegate.swift
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         
         
-        // Hello Dharmesh Here...
+        // Hello Dharmesh Done by tushar
         // Tushar
         // Hi denish
     }
